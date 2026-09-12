38b8955292359e04512afe3fae6d6a7117155393
SamWaku <samuelwaku1st@gmail.com>
2026-09-12 08:47:20 +0200

    chore(ref): palindrome

--- CSharpCoreConcepts/Concepts/Extensions/ExtentionMembers.cs
+++ CSharpCoreConcepts/Concepts/Extensions/ExtentionMembers.cs
@@ -2,5 +2,9 @@ namespace CSharpCoreConcepts.Concepts.Extensions;
 
 public static class ExtentionMembers
 {
-    public static bool IsPalindrome(this string s) => s.Reverse().SequenceEqual(s.Reverse());
+    public static bool IsPalindrome(this string s)
+    {
+        var reversed = new string(s.Reverse().ToArray());
+        return s.Equals(reversed, StringComparison.OrdinalIgnoreCase);
+    }
 }
