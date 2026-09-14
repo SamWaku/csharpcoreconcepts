9de29ea5aea5d14e5de5c4aff0f545e6e3e714ea
SamWaku <samuelwaku1st@gmail.com>
2026-09-14 09:14:07 +0200

    push

--- CSharpCoreConcepts/Program.cs
+++ CSharpCoreConcepts/Program.cs
@@ -5,8 +5,6 @@ public class Program
 {
     public static void Main()
     {
-        Polymorphism anotherString = new AnotherPolymorphism();
-        Console.WriteLine(anotherString.SayHello());
         Console.WriteLine($"Palindrome: {"sam".IsPalindrome()}");
     }
 }
